5ba96a6aa8244b07ee0b3a0e6908cfaebf525622
liuzeyu12a <515801535@qq.com>
2020-05-19 17:08:23 +0800

    添加first到本地仓库

---
+++ git-test/src/First.java
@@ -0,0 +1,10 @@
+public class First {
+
+    public static void main(String[] args) {
+        System.out.println("1111111111111");
+        System.out.println("1111111111111");
+        System.out.println("1111111111111");
+        System.out.println("1111111111111");
+        System.out.println("1111111111111");
+    }
+}
